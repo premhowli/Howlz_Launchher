aa2cf96e6688bcc2069037d8b8aa43f6f7f25a32
Premangshu Howli <Premangshu Howli>
2016-05-01 18:05:26 +0530

    Initial commit

---
+++ app/src/main/java/com/howlzzz/howlzlaunchher/AppDetail.java
@@ -0,0 +1,10 @@
+package com.howlzzz.howlzlaunchher;
+import android.graphics.drawable.Drawable;
+/**
+ * Created by HOWLZZZ on 29-04-2016.
+ */
+public class AppDetail {
+    CharSequence label;
+    CharSequence name;
+    Drawable icon;
+}
---
+++ app/src/main/java/com/howlzzz/howlzlaunchher/AppsListActivity.java
@@ -0,0 +1,111 @@
+package com.howlzzz.howlzlaunchher;
+
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.pm.PackageManager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppsListActivity extends AppCompatActivity {
+    private PackageManager manager;
+    private List<AppDetail> apps;
+    private void loadApps(){
+        manager = getPackageManager();
+        apps = new ArrayList<AppDetail>();
+
+
+        Intent i = new Intent(Intent.ACTION_MAIN, null);
+        i.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        List<ResolveInfo> availableActivities = manager.queryIntentActivities(i, 0);
+        for(ResolveInfo ri:availableActivities){
+            AppDetail app = new AppDetail();
+            app.label = ri.loadLabel(manager);
+            app.name = ri.activityInfo.packageName;
+            app.icon = ri.activityInfo.loadIcon(manager);
+            apps.add(app);
+        }
+    }
+    private ListView list;
+    private void loadListView(){
+        list = (ListView)findViewById(R.id.apps_list);
+
+        ArrayAdapter<AppDetail> adapter = new ArrayAdapter<AppDetail>(this,
+                R.layout.list_item,
+                apps) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if(convertView == null){
+                    convertView = getLayoutInflater().inflate(R.layout.list_item, null);
+                }
+
+                ImageView appIcon = (ImageView)convertView.findViewById(R.id.item_app_icon);
+                appIcon.setImageDrawable(apps.get(position).icon);
+
+                TextView appLabel = (TextView)convertView.findViewById(R.id.item_app_label);
+                appLabel.setText(apps.get(position).label);
+
+                TextView appName = (TextView)convertView.findViewById(R.id.item_app_name);
+                appName.setText(apps.get(position).name);
+
+                return convertView;
+            }
+        };
+
+        list.setAdapter(adapter);
+    }
+    private void addClickListener(){
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> av, View v, int pos,
+                                    long id) {
+                Intent i = manager.getLaunchIntentForPackage(apps.get(pos).name.toString());
+                AppsListActivity.this.startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apps_list);
+
+        loadApps();
+        loadListView();
+        addClickListener();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_apps_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
